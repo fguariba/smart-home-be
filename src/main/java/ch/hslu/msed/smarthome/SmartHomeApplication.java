12c6ac1362c17ba7ccc76c2d65e6b2094543e5ad
fguariba <franciscoguariba@hotmail.com>
2024-01-02 22:15:14 +0100

    Initial commit

---
+++ src/main/java/ch/hslu/msed/smarthome/SmartHomeApplication.java
@@ -0,0 +1,13 @@
+package ch.hslu.msed.smarthome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/hslu/msed/smarthome/SmartHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.hslu.msed.smarthome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
