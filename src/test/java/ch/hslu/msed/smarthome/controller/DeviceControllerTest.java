a971f9158135bbf37638939a1283d84b391b5dd4
fguariba <franciscoguariba@hotmail.com>
2024-01-11 21:42:03 +0100

    SMART-7: Add initial tests for the controller

--- src/test/java/ch/hslu/msed/smarthome/controller/DeviceControllerTest.java
+++ src/test/java/ch/hslu/msed/smarthome/controller/DeviceControllerTest.java
@@ -8,20 +8,19 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class SmartHomeControllerTest {
+class DeviceControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void getWeatherInfo_successfully() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/weather-info").accept(MediaType.APPLICATION_JSON))
+    void getDevices_successfully() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/devices").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Hello")));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 }
---
+++ src/test/java/ch/hslu/msed/smarthome/controller/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package ch.hslu.msed.smarthome.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getHomes_successfully() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/homes").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+}
