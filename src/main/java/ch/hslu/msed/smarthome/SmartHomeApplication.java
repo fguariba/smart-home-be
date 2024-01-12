82ad3304c6065b50d6308071dca0cc7ed79e1c9f
fguariba <franciscoguariba@hotmail.com>
2024-01-12 15:44:03 +0100

    SMART-7: Fix Swagger UI error

--- src/main/java/ch/hslu/msed/smarthome/SmartHomeApplication.java
+++ src/main/java/ch/hslu/msed/smarthome/SmartHomeApplication.java
@@ -1,12 +1,9 @@
 package ch.hslu.msed.smarthome;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Test", version = "1.0.0", description = "test"))
 public class SmartHomeApplication {
 
     public static void main(String[] args) {
