0484baa9c0c9b10f1851157544d5ddb0721db896
fguariba <franciscoguariba@hotmail.com>
2024-01-12 10:01:08 +0100

    SMART-7: Fix test issues

--- src/test/java/ch/hslu/msed/smarthome/controller/UserControllerTest.java
+++ src/test/java/ch/hslu/msed/smarthome/controller/UserControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("local")
 class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
